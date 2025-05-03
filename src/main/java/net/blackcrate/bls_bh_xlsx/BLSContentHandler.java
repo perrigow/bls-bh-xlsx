a952180b33bc942f2ed4e34a92ef1ed924351d3b
Wayne Perrigo <83511562+perrigow@users.noreply.github.com>
2025-05-03 09:45:39 -0400

    updated game regex

--- src/main/java/net/blackcrate/bls_bh_xlsx/BLSContentHandler.java
+++ src/main/java/net/blackcrate/bls_bh_xlsx/BLSContentHandler.java
@@ -17,7 +17,7 @@ public class BLSContentHandler extends DefaultHandler {
 
     private static final String REGEX_WEEK = "^([1-9]|[12][0-9])$";
     private static final String REGEX_DATE = "^(0?[1-9]|1[012])/(0?[1-9]|[12][0-9]|3[01])/(\\d{2})$";
-    private static final String REGEX_GAME = "(^[1,2]{0,1}[1-9]{0,1}[0-9]$)|(300)";
+    private static final String REGEX_GAME = "^([1,2]{0,1}[0-9]{1,2}|300)$";
 
     private final StringBuilder currentText;
     private boolean isHdcp;
